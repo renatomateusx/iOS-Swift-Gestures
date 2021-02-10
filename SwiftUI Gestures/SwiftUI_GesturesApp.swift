7fec2268502b36491ea5cc7c10885422b3671851
Renato Mateus <renatomateusx@gmail.com>
2021-02-09 21:21:51 -0300

    Initial Commit

---
+++ SwiftUI Gestures/SwiftUI_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GesturesApp.swift
+//  SwiftUI Gestures
+//
+//  Created by Renato Mateus on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
