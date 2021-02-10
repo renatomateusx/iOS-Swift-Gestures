39cb972f51eb9eb2a1f8823cecb05d288b50b014
Renato Mateus <renatomateusx@gmail.com>
2021-02-10 07:08:19 -0300

    Keep this project on my git for the futures research

--- SwiftUI Gestures/ContentView.swift
+++ SwiftUI Gestures/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState var isLongPressed = false
+    
+    @State var offset: CGSize = .zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        let longPressGesture = LongPressGesture()
+            .updating($isLongPressed){
+                newValue, state, transaction in state = newValue
+            }
+        let dragGesture = DragGesture()
+            .onChanged{value in self.offset = value.translation}
+        
+        VStack {
+            Circle()
+                .frame(width:150, height: 150, alignment: .center)
+                .foregroundColor(isLongPressed ? .orange : .red)
+                .scaleEffect(isLongPressed ? 2 : 1)
+                .offset(x: offset.width, y: offset.height)
+                .gesture(longPressGesture)
+                .animation(.spring())
+        }
+                
+        VStack {
+            Circle()
+                .frame(width:150, height: 150, alignment: .center)
+                .foregroundColor(.blue)
+                .offset(x: offset.width, y: offset.height)
+                .gesture(dragGesture)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12")
     }
 }
